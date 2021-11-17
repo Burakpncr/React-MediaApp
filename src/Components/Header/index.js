ca17ace311fcdfa564328b339e91b9df39655fa5
Burak Pancar <83040277+Burakpncr@users.noreply.github.com>
2021-11-17 15:35:11 +0300

    style : Header designed

---
+++ src/Components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="Header-Container">
+      <div className="Header-Wrapper">
+      <div className="logo">
+        ActionPost
+      </div>
+      <div className="Menu">
+        Menü
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
