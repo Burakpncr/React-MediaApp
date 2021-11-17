be21f942c6c38e272e3ae52dabc88754be167099
Burak Pancar <83040277+Burakpncr@users.noreply.github.com>
2021-11-17 16:58:13 +0300

    style:Hamburger Menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import {useState} from 'react'
 import './Header.css'
+import { AiOutlineMenu } from "react-icons/ai"
+import { AiOutlineClose } from "react-icons/ai"
+
 
 function Header() {
+  const [openMenu, setopenMenu] = useState(false)
   return (
     <div className="Header-Container">
       <div className="Header-Wrapper">
       <div className="logo">
-        ActionPost
+       <h1> ActionPost </h1>
       </div>
       <div className="Menu">
-        Menü
+        <div className="Nav-icon"> 
+        {
+          openMenu ?
+             <div className="Hamburger-Menu"> 
+             <button className="hamburger-icon" onClick={() => setopenMenu(false)}><AiOutlineClose /></button> 
+             <a href="todos"> Todos </a>
+             <a href="posts"> Posts </a>
+             <a href="albums"> Albums </a>
+             <a href="login"> Users </a>
+             </div> 
+           : 
+           <button className="hamburger-icon" onClick={() => setopenMenu(true)}><AiOutlineMenu /></button> 
+        }
+        </div>
+        <div className="Link">
+        <a href="todos"> Todos </a>
+        <a href="posts"> Posts </a>
+        <a href="albums"> Albums </a>
+        <a href="login"> Users </a>
+        </div>
       </div>
       </div>
     </div>
