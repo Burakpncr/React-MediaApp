a239b8fea647f275ef6c8874ed5cf724a83c4b72
Burak Pancar <83040277+Burakpncr@users.noreply.github.com>
2022-10-20 13:09:15 +0300

    Todo card style updated

--- src/Pages/Todos/index.js
+++ src/Pages/Todos/index.js
@@ -1,4 +1,4 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 import "./Todos.css"
 
@@ -6,39 +6,38 @@ function Todos() {
   const [Todos, setTodos] = useState()
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(response => {
-      setTodos(response.data)
-    })
-    .catch(error => {
+      .then(response => {
+        setTodos(response.data)
+      })
+      .catch(error => {
         console.log(error);
-    });
-  },[])
+      });
+  }, [])
   console.log(Todos)
   return (
     <div className='Todos-Container'>
       <div className='Todos-Wrapper'>
         {
-          Todos ? 
-          Todos.map(todo => {
-            return(
-              <div className='Todos-Card'>
-                <div>
-               Todo : {todo.title}
-               </div>
-               <div>
-                Status : {todo.completed ? "Completed" : "Not Completed"}
-                 </div>
-             </div> 
-            )
-          }) : <div>
-            <p class="placeholder-glow">
-  <span class="placeholder col-12"></span>
-</p>
+          Todos ?
+            Todos.map(todo => {
+              return (
+                <div class="card todos-card-wrapper" style={{marginBottom:'10px'}}>
+                  <div class="card-body">
+                    <h5 class="card-title">Todo</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{todo.title}</h6>
+                    <p class="card-text"> {todo.completed ? "Completed" : "Not Completed"}</p>
+                  </div>
+                </div>
+              )
+            }) : <div>
+              <p class="placeholder-glow">
+                <span class="placeholder col-12"></span>
+              </p>
 
-<p class="placeholder-wave">
-  <span class="placeholder col-12"></span>
-</p>
-          </div>
+              <p class="placeholder-wave">
+                <span class="placeholder col-12"></span>
+              </p>
+            </div>
         }
       </div>
 
